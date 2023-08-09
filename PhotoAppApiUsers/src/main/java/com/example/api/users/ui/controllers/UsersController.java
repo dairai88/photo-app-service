d14fe3d0bbe14a616de283370cf21a6a34f9da10
dalei <sundalei2011@163.com>
2023-08-09 22:22:39 +0800

    feature: refactor endpoint to include port info

--- PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,5 @@
 package com.example.api.users.ui.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
