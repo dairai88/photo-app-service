3bc58849723e49c04761b17c6ae090001be4545b
dalei <sundalei2011@163.com>
2023-08-09 22:22:14 +0800

    feature: refactor endpoint to include port info

--- PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.example.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    private final Environment env;
+
+    public UsersController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + env.getProperty("local.server.port");
     }
 }
