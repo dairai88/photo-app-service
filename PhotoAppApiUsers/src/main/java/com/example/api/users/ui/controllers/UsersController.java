773bffb4681515997d684f74163bdb63ed9bbc5b
daleisun <sundalei2011@163.com>
2023-08-19 12:37:01 +0800

    feature: create user endpoint

--- PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
@@ -2,8 +2,14 @@ package com.example.api.users.ui.controllers;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api.users.ui.model.CreateUserRequestModel;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -18,5 +24,10 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        return "Create user method is called";
     }
 }
