f04c8f237c7223a7f37129862e5ff90a59b3b80d
daleisun <sundalei2011@163.com>
2023-08-20 19:38:06 +0800

    feature: create user

--- PhotoAppApiUsers/src/main/java/com/example/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/service/UsersServiceImpl.java
@@ -1,15 +1,37 @@
 package com.example.api.users.service;
 
+import com.example.api.users.data.UserEntity;
+import com.example.api.users.data.UsersRepository;
 import com.example.api.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     @Override
     public UserDto createUser(UserDto userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
-        return null;
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+
+        usersRepository.save(userEntity);
+
+        UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+        
+        return returnValue;
     }
     
 }
--- PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,12 @@
 package com.example.api.users.ui.controllers;
 
+import com.example.api.users.service.UsersService;
+import com.example.api.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.users.ui.model.CreateUserRequestModel;
+import com.example.api.users.ui.model.CreateUserResponseModel;
 
 import jakarta.validation.Valid;
 
@@ -16,9 +23,11 @@ import jakarta.validation.Valid;
 public class UsersController {
 
     private final Environment env;
+    private final UsersService usersService;
 
-    public UsersController(Environment env) {
+    public UsersController(Environment env, UsersService usersService) {
         this.env = env;
+        this.usersService = usersService;
     }
 
     @GetMapping("/status/check")
@@ -27,7 +36,15 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
-        return "Create user method is called";
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        UserDto createdUser = usersService.createUser(userDto);
+
+        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 }
