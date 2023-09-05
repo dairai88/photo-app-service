5b58d2ff42c6ce47ecea902ef95f298612c69098
daleisun <sundalei2011@163.com>
2023-09-05 23:08:55 +0800

    feature: check spring bus feature
    1. change controller to display a property greeting.user
    2. move properties from application yaml to github config
    3. change greeting.user to different value
    4. use busrefresh endpoint to refresh config and check greeting.user changed in status endpoint
    5. busrefresh refers to POST http://localhost:8012/actuator/busrefresh

--- PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/ui/controllers/UsersController.java
@@ -32,7 +32,7 @@ public class UsersController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working on port " + env.getProperty("local.server.port");
+        return "Working on port " + env.getProperty("local.server.port") + ", Welcome " + env.getProperty("greeting.user");
     }
 
     @PostMapping
