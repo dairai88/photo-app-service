22bf3dff8e1b18e83da1d877187b63e1b8001180
daleisun <sundalei2011@163.com>
2023-09-24 15:29:04 +0800

    feature: enable circuitbreaker for albums-ws service

--- PhotoAppApiUsers/src/main/java/com/example/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/example/api/users/data/AlbumsServiceClient.java
@@ -1,16 +1,27 @@
 package com.example.api.users.data;
 
-import java.util.List;
-
+import com.example.api.users.ui.model.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.example.api.users.ui.model.AlbumResponseModel;
+import java.util.List;
 
 @FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
-    @GetMapping("/users/{id}/albumss")
-    public List<AlbumResponseModel> getAlbums(@PathVariable("id") String id);
+    Logger LOG = LoggerFactory.getLogger(AlbumsServiceClient.class);
+
+    @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
+    List<AlbumResponseModel> getAlbums(@PathVariable("id") String id);
+
+    default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exception) {
+        LOG.error("Parameter {}", id);
+        LOG.error("exception {}", exception.getLocalizedMessage());
+        return List.of();
+    }
 }
