3d94643264490400545dc37ec740f7af4aa7f8bc
Dalei Sun <daleisun@Daleis-MacBook-Pro.local>
2024-02-21 21:19:17 +0800

    feature: add testcontainers for rabbitmq

--- PhotoAppApiConfigServer/src/test/java/com/example/api/configserver/PhotoAppApiConfigServerApplicationTests.java
+++ PhotoAppApiConfigServer/src/test/java/com/example/api/configserver/PhotoAppApiConfigServerApplicationTests.java
@@ -1,10 +1,34 @@
 package com.example.api.configserver;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 class PhotoAppApiConfigServerApplicationTests {
+
+	static RabbitMQContainer rabbit = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.7.25-management-alpine"));
+
+	@BeforeAll
+	static void beforeAll() {
+		rabbit.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		rabbit.stop();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.rabbitmq.host", rabbit::getHost);
+		registry.add("spring.rabbitmq.port", rabbit::getAmqpPort);
+	}
 
 	@Test
 	void contextLoads() {
